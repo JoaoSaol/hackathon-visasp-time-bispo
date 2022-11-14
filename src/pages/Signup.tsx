86333b0db1c4768602fc7be15c43420463c63a6d
Habner <habnergerotto@gmail.com>
2022-11-14 07:08:03 -0300

    feat: add Login and Signup pages

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,40 @@
+import { Link, useNavigate } from 'react-router-dom'
+
+import { Button } from '../components/Button'
+import { Input } from '../components/Input'
+
+import TorcidaPNG from '../assets/torcida.png'
+import LogoWhiteSVG from '../assets/logowhite.svg'
+import TelephoneSVG from '../assets/telephone.svg'
+import LockSVG from '../assets/lock.svg'
+import NameSVG from '../assets/name.svg'
+import CpfSVG from '../assets/cpf.svg'
+
+export function Signup () {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => navigate('/meu-painel');
+
+  return (
+    <div style={{display: 'grid', width: '100vw', height: '100vh', gridTemplateColumns: '6fr 4fr', overflow: 'hidden'}}>
+      <div>
+        <img src={TorcidaPNG} style={{width: '100%', height: '100%', objectFit: 'cover'}} />
+        <img src={LogoWhiteSVG} style={{position: 'absolute', top: '56px', left: '160px'}} />
+      </div>
+      <div>
+        <form style={{display: 'grid', padding: '48px', height: '100vh', alignContent: 'center', justifyContent: 'center', gap: '48px'}}>
+          <h2 style={{fontSize: '28px', fontWeight: '500', margin: '0'}}>Cadastrar minha conta</h2>
+          <div style={{display: 'grid', gap: '16px'}}>
+            <Input type='text' placeholder='Nome Completo' icon={NameSVG} />
+            <Input type='tel' placeholder='Número do WhatsApp' icon={TelephoneSVG} />
+            <Input type='text' placeholder='CPF' icon={CpfSVG} />
+            <Input type='password' placeholder='Senha' icon={LockSVG} />
+          </div>
+          <Button type='button' onClick={handleClick}>Cadastrar</Button>
+          <span style={{justifySelf: 'center'}}>Já possui uma conta? <Link to='/login' style={{fontWeight: '500', textDecoration: 'underline'}}>Entre</Link></span>
+        </form>
+      </div>
+    </div>
+  );
+}
